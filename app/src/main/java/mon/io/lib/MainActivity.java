0ddbfbff717cab5ba7a3fb58dc80ddce506bf5e1
thuan <thuanhbu2@gmail.com>
2017-02-15 10:14:17 +0700

    fix

--- app/src/main/java/mon/io/lib/MainActivity.java
+++ app/src/main/java/mon/io/lib/MainActivity.java
@@ -2,14 +2,8 @@ package mon.io.lib;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.RelativeLayout;
-
-import io.mon.NotificationBox;
 
 public class MainActivity extends AppCompatActivity {
-
-    private NotificationBox notificationBox;
-    private RelativeLayout activity_main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
