2750a7df0d3ecb458e6c8a84460fcc967a596cbc
thuan <thuanhbu2@gmail.com>
2017-02-15 10:02:22 +0700

    fix

--- app/src/main/java/mon/io/lib/MainActivity.java
+++ app/src/main/java/mon/io/lib/MainActivity.java
@@ -2,10 +2,8 @@ package mon.io.lib;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.RelativeLayout;
 
-import es.dmoral.prefs.Prefs;
 import io.mon.NotificationBox;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,19 +15,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initView();
-        Prefs.with(getApplicationContext()).writeBoolean("enable_notibox", true);
-        notificationBox.Build();
-        notificationBox.OnbackPress(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //code here
-            }
-        });
+
     }
 
-    private void initView() {
-        notificationBox = (NotificationBox) findViewById(R.id.notificationBox);
-        activity_main = (RelativeLayout) findViewById(R.id.activity_main);
-    }
+
 }
